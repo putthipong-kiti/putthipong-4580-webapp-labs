b711e51a4a2e901602ed33000d2edd39b8f4b1b5
Putthipong21 <putthipong.ki@kkumail.com>
2024-09-23 16:29:17 +0700

    edit exercise 21 problem 2

--- a.js
+++
@@ -1,8 +0,0 @@
-import inquirer from "inquirer";
-var questions = [
-    { type:'input', name:'username', message:'Username:'},
-    { type:'password', name:'password', message:'Password   :'}
-]
-inquirer.prompt(questions).then(answer => {
-    console.log(`Hi ${answer.username}!`)
-})
